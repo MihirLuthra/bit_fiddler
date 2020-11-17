8b67912ab0b3af0d23a578c1eef2f1e3a14b09cc
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-11-17 06:09:08 +0530

    accept var args for multi bit opertions

--- src/bit_fiddle_macros.rs
+++ src/bit_fiddle_macros.rs
@@ -7,16 +7,29 @@
 /// let mut bitmap: u64 = 0;
 ///
 /// set_bit!(bitmap, 0);
-/// set_bit!(bitmap, 2);
-/// set_bit!(bitmap, 4);
+/// set_bit!(bitmap, 2, 4);
 ///
 /// assert_eq!(bitmap, 0b10101);
+///
+/// // use `get` as 2nd arg to return the resultant bitmap
+///
+/// let set_bitmap = set_bit!(bitmap, get, 3);
+///
+/// assert_eq!(set_bitmap, 0b11101);
+/// assert_eq!(bitmap, 0b10101);
+///
+/// let set_bitmap = set_bit!(0b0011, get, 2, 3);
+///
+/// assert_eq!(set_bitmap, 0b1111);
 /// ```
 #[macro_export]
 macro_rules! set_bit {
-    ($bitmap: ident, $bit_pos: tt) => {
-        $bitmap |= 1 << $bit_pos;
-    }
+    ($bitmap: tt, get, $( $bit_pos: tt),*) => {
+        $bitmap | $( (1 << $bit_pos) | )* 0;
+    };
+    ($bitmap: ident, $( $bit_pos: tt),* ) => {
+        $bitmap |= $( (1 << $bit_pos) | )* 0;
+    };
 }
 
 /// Unsets bit at the given position in bitmap.
@@ -25,22 +38,32 @@ macro_rules! set_bit {
 /// ```
 /// use bit_fiddler::unset_bit;
 /// 
-/// let mut bitmap: u8 = 0b_1000_0101;
-///
+/// let mut bitmap = 0b0000_0111;
+/// 
 /// unset_bit!(bitmap, 0);
-/// assert_eq!(bitmap, 0b_1000_0100);
+/// unset_bit!(bitmap, 1, 2);
+/// 
+/// assert_eq!(bitmap, 0);
+/// 
+/// let mut bitmap = 0b0000_0111;
 ///
-/// unset_bit!(bitmap, 2);
-/// assert_eq!(bitmap, 0b_1000_0000);
-///
-/// unset_bit!(bitmap, 7);
-/// assert_eq!(bitmap, 0b_0000_0000);
+/// // use `get` as 2nd arg to return the resultant bitmap
+/// 
+/// let res_bitmap = unset_bit!(bitmap, get, 0, 1, 2);
+/// 
+/// assert_eq!(res_bitmap, 0);
+/// assert_eq!(bitmap, 0b0000_0111);
+/// 
+/// assert_eq!(unset_bit!(0b111, get, 0, 1, 2), 0);
 /// ```
 #[macro_export]
 macro_rules! unset_bit {
-    ($bitmap: ident, $bit_pos: tt) => {
-        $bitmap &= !(1 << $bit_pos);
-    }
+    ($bitmap: tt, get, $( $bit_pos: tt),*) => {
+        $bitmap & !( $( (1 << $bit_pos) | )* 0 );
+    };
+    ($bitmap: ident, $( $bit_pos: tt),* ) => {
+        $bitmap &= !( $( (1 << $bit_pos) | )* 0 );
+    };
 }
 
 /// Unsets bit at the given position in bitmap.
@@ -49,35 +72,50 @@ macro_rules! unset_bit {
 /// ```
 /// use bit_fiddler::toggle_bit;
 /// 
-/// let mut bitmap: u8 = 0b_1000_0101;
-///
+/// let mut bitmap = 0b0000_0111;
+/// 
 /// toggle_bit!(bitmap, 0);
-/// assert_eq!(bitmap, 0b_1000_0100);
-///
-/// toggle_bit!(bitmap, 1);
-/// assert_eq!(bitmap, 0b_1000_0110);
-///
-/// toggle_bit!(bitmap, 6);
-/// assert_eq!(bitmap, 0b_1100_0110);
+/// toggle_bit!(bitmap, 7, 5);
+/// 
+/// assert_eq!(bitmap, 0b1010_0110);
+/// 
+/// let res_bitmap = toggle_bit!(bitmap, get, 0, 1, 2);
+/// 
+/// assert_eq!(bitmap, 0b1010_0110);
+/// assert_eq!(res_bitmap, 0b1010_0001);
+/// 
+/// assert_eq!(toggle_bit!(0b101, get, 0, 1, 2), 0b010);
 /// ```
 #[macro_export]
 macro_rules! toggle_bit {
-    ($bitmap: ident, $bit_pos: tt) => {
-        $bitmap ^= 1 << $bit_pos;
-    }
+    ($bitmap: tt, get, $( $bit_pos: tt),*) => {
+        $bitmap ^ ( $( (1 << $bit_pos) | )* 0 );
+    };
+    ($bitmap: ident, $( $bit_pos: tt),* ) => {
+        $bitmap ^= $( (1 << $bit_pos) | )* 0;
+    };
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn set_bit() {
-        let mut bitmap = 0;
-
-        set_bit!(bitmap, 0);
-        set_bit!(bitmap, 1);
-        set_bit!(bitmap, 2);
-
-        assert_eq!(bitmap, 0b111);
+        let mut bitmap: u64 = 0;
+		set_bit!(bitmap, 0);
+		set_bit!(bitmap, 2, 4);
+		
+		assert_eq!(bitmap, 0b10101);
+		
+		// use `get` as 2nd arg to return the set bitmap
+		
+		let set_bitmap = set_bit!(bitmap, get, 3);
+		
+		assert_eq!(set_bitmap, 0b11101);
+		assert_eq!(bitmap, 0b10101);
+		
+		let set_bitmap = set_bit!(0b0011, get, 2, 3);
+		
+		assert_eq!(set_bitmap, 0b1111);
     }
 
     #[test]
@@ -85,10 +123,18 @@ mod tests {
         let mut bitmap = 0b0000_0111;
 
         unset_bit!(bitmap, 0);
-        unset_bit!(bitmap, 1);
-        unset_bit!(bitmap, 2);
+        unset_bit!(bitmap, 1, 2);
 
         assert_eq!(bitmap, 0);
+
+        let mut bitmap = 0b0000_0111;
+
+        let res_bitmap = unset_bit!(bitmap, get, 0, 1, 2);
+
+        assert_eq!(res_bitmap, 0);
+        assert_eq!(bitmap, 0b0000_0111);
+
+        assert_eq!(unset_bit!(0b111, get, 0, 1, 2), 0);
     }
 
     #[test]
@@ -96,9 +142,15 @@ mod tests {
         let mut bitmap = 0b0000_0111;
 
         toggle_bit!(bitmap, 0);
-        toggle_bit!(bitmap, 7);
-        toggle_bit!(bitmap, 5);
+        toggle_bit!(bitmap, 7, 5);
 
         assert_eq!(bitmap, 0b1010_0110);
+
+        let res_bitmap = toggle_bit!(bitmap, get, 0, 1, 2);
+
+        assert_eq!(bitmap, 0b1010_0110);
+        assert_eq!(res_bitmap, 0b1010_0001);
+
+        assert_eq!(toggle_bit!(0b101, get, 0, 1, 2), 0b010);
     }
 }
