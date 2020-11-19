63cfc5a8d9b96c822f92c3615bb23a6f1a563a54
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-11-19 15:25:14 +0530

    add docs for max_bits!()

--- src/bit_fiddle_macros/max_bits.rs
+++ src/bit_fiddle_macros/max_bits.rs
@@ -1,5 +1,18 @@
 /// For getting total bit count
 /// by type or identifier.
+///
+/// # Example
+///
+/// ```
+/// use bit_fiddler::max_bits;
+///
+/// assert_eq!(max_bits!(type = i32), 32);
+/// assert_eq!(max_bits!((5 as u64)), 64);
+///
+/// let bitmap: u8 = 0;
+///
+/// assert_eq!(max_bits!(bitmap), 8);
+/// ```
 #[macro_export]
 macro_rules! max_bits {
     ($bitmap: tt) => {
